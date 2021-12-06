e28a1f4b72a66f6d2e6bf79d467092a84779b142
Willy Wijaya <86144589+Willy-Wi@users.noreply.github.com>
2021-12-06 10:48:51 +0700

    Added Basic HTML Tags and CSS. Made an Express - MySQL script

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+// import mysql2 from "mysql2";
+
+const hostname = "localhost";
+const port = 3036;
+
+const app = express();
+// const conn = mysql2.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "",
+// });
+
+// conn.connect((err) => {
+//     if (err) throw err;
+//     console.log("MySQL WorkBench is Connected...!");
+// });
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile("public/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server Running at ${hostname}:${port}`);
+});
