8e76d1a1ae8abb724dc394cfe6b093599f52ffc7
Willy Wijaya <wijayawilly2003@gmail.com>
2021-12-06 13:55:37 +0700

    Added Advanced CSS Styling (Including Hover Animation). Added a javascript for displaying Current Date Time. Added More HTML tags and contents.

---
+++ public/script.js
@@ -0,0 +1,40 @@
+const currenDate = new Date();
+
+const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const nth = (d) => {
+    if (d > 3 && d < 21) return "th";
+    switch (d % 10) {
+        case 1:
+            return "st";
+        case 2:
+            return "nd";
+        case 3:
+            return "rd";
+        default:
+            return "th";
+    }
+};
+
+let dateTime = nth(currenDate.getDate());
+
+let time = `${
+    month[currenDate.getMonth()]
+} ${currenDate.getDate()}${dateTime} ${currenDate.getFullYear()}`;
+
+let date = document.getElementById("date");
+
+date.insertAdjacentHTML("beforeend", time);
