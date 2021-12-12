25e6e29ecd5cb2a249a992bd3ce986453a2366de
Willy Wijaya <wijayawilly2003@gmail.com>
2021-12-12 16:07:16 +0700

    Add display database data to the main page
    Fixed some bugs.
    Add logout routing.

--- routes/pages.js
+++ routes/pages.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const bcrypt = require("bcrypt");
 const mysql2 = require("mysql2");
-const { register, login, logout } = require("../controllers/authCon");
+const { register, login, main } = require("../controllers/authCon");
 
 const router = Router();
 
@@ -12,20 +12,41 @@ const conn = mysql2.createConnection({
     database: "test_db_home",
 });
 
-router.get("/", (req, res) => {
-    res.render("home");
+router.get("/", async (req, res) => {
+    let sql = "SELECT * FROM comments";
+
+    let query = await new Promise((resolve, reject) => {
+        conn.query(sql, (err, result) => {
+            if (err) reject(err);
+
+            resolve(result);
+        });
+    });
+
+    if (!req.session.authenticated)
+        return res.render("home", { login: false, result: query });
+
+    res.render("home", { result: query, login: true });
 });
 
 router.get("/register", (req, res) => {
-    res.render("register", { message: null });
+    res.render("register");
 });
 
 router.get("/login", (req, res) => {
-    res.render("login", { message: null });
+    res.render("login");
 });
 
+router.get("/logout", (req, res) => {
+    if (req.session.authenticated) {
+        req.session.authenticated = false;
+        return res.redirect("/");
+    }
+    res.render("login", { message: "You are not logged in!" });
+});
+
+router.post("/", main);
 router.post("/register", register);
 router.post("/login", login);
-router.post("/logout", logout);
 
 module.exports = router;
