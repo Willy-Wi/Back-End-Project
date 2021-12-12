607556a6302c9623bc7211e55099a6b6b8847961
Willy Wijaya <wijayawilly2003@gmail.com>
2021-12-12 16:03:57 +0700

    Add more data validation and database data fetch
    I added a code to display all the messages by fetching the data from a database.
    I also fixed a few bugs and issues.

--- controllers/authCon.js
+++ controllers/authCon.js
@@ -1,5 +1,6 @@
 const mysql2 = require("mysql2");
 const bcrypt = require("bcrypt");
+const isEligibleRequest = require("express-fileupload/lib/isEligibleRequest");
 
 const conn = mysql2.createConnection({
     host: "localhost",
@@ -7,6 +8,28 @@ const conn = mysql2.createConnection({
     password: "",
     database: "test_db_home",
 });
+
+const main = (req, res) => {
+    const { message } = req.body;
+
+    if (!req.session.authenticated)
+        return res.render("login", { message: "You have not logged in!" });
+
+    let sql = "INSERT INTO comments SET ?";
+
+    let user = req.session.user.username;
+
+    let data = {
+        username: user,
+        comments_text: message,
+    };
+
+    conn.query(sql, [data], (err, result) => {
+        if (err) throw err;
+
+        res.redirect("/");
+    });
+};
 
 const register = async (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
@@ -40,11 +63,10 @@ const register = async (req, res) => {
         password: hashedPassword,
     };
 
-    conn.query(sql, data, (err, res) => {
+    conn.query(sql, data, (err, result) => {
         if (err) throw err;
-
-        res.render("register", { message: "Successfully registered" });
     });
+    res.render("register", { message: "Successfully registered" });
 };
 
 const login = async (req, res) => {
@@ -60,19 +82,22 @@ const login = async (req, res) => {
         });
     });
 
-    if (query.length === 0) {
+    if (query.length < 1) {
         return res.render("login", { message: "That user does not exist!" });
     }
 
     let checkPassword = await bcrypt.compare(password, query[0].password);
 
-    if (checkPassword) {
+    if (!checkPassword)
+        return res.render("login", { message: "Invalid Password!" });
+
+    if (!req.session.authenticated) {
+        req.session.authenticated = true;
+        req.session.user = { username };
         return res.render("login", { message: "Successfully Logged in!" });
-    } else {
-        return res.render("login", { message: "Invalid password!" });
     }
+
+    res.render("login", { message: "You're already logged in!" });
 };
 
-const logout = (req, res) => {};
-
-module.exports = { register, login, logout };
+module.exports = { register, login, main };
