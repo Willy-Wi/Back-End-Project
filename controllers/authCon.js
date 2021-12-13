ac440dbee9dfad13561f50031c0202e56b151496
Willy Wijaya <wijayawilly2003@gmail.com>
2021-12-13 22:22:15 +0700

    Make changes to the new database

--- controllers/authCon.js
+++ controllers/authCon.js
@@ -1,6 +1,7 @@
 const mysql2 = require("mysql2");
 const bcrypt = require("bcrypt");
-const isEligibleRequest = require("express-fileupload/lib/isEligibleRequest");
+const { validationResult } = require("express-validator");
+// const isEligibleRequest = require("express-fileupload/lib/isEligibleRequest");
 
 const conn = mysql2.createConnection({
     host: "localhost",
@@ -11,20 +12,19 @@ const conn = mysql2.createConnection({
 
 const main = (req, res) => {
     const { message } = req.body;
+    const { user_id } = req.session.user;
 
     if (!req.session.authenticated)
         return res.render("login", { message: "You have not logged in!" });
 
-    let sql = "INSERT INTO comments SET ?";
-
-    let user = req.session.user.username;
+    let sql = "INSERT INTO posts SET ?";
 
     let data = {
-        username: user,
-        comments_text: message,
+        user_id: user_id,
+        post_content: message,
     };
 
-    conn.query(sql, [data], (err, result) => {
+    conn.query(sql, data, (err, result) => {
         if (err) throw err;
 
         res.redirect("/");
@@ -32,16 +32,24 @@ const main = (req, res) => {
 };
 
 const register = async (req, res) => {
-    const { username, email, password, confirmPassword } = req.body;
+    const errors = validationResult(req);
 
-    if (password !== confirmPassword) {
-        return res.render("register", { message: "Password does not match!" });
+    if (!errors.isEmpty()) {
+        return res.render("register", { error: errors.array() });
     }
 
-    let sql = "SELECT email FROM users WHERE email = ?";
+    const { username, fullname, email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+        return res.render("register", {
+            message: "Password does not match!",
+        });
+    }
+
+    let sql = `SELECT email, username FROM users WHERE email = '${email}' OR username = '${username}'`;
 
     let query = await new Promise((resolve, reject) => {
-        conn.query(sql, email, (err, result) => {
+        conn.query(sql, (err, result) => {
             if (err) reject(err);
 
             resolve(result);
@@ -49,9 +57,15 @@ const register = async (req, res) => {
     });
 
     if (query.length > 0) {
-        return res.render("register", {
-            message: "That email is already registered!",
-        });
+        if (query[0].email) {
+            return res.render("register", {
+                message: "That email is already registered!",
+            });
+        } else if (query[0].password) {
+            return res.render("register", {
+                message: "That username already exist!",
+            });
+        }
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -59,6 +73,7 @@ const register = async (req, res) => {
     sql = "INSERT INTO users SET ?";
     let data = {
         username: username,
+        name: fullname,
         email: email,
         password: hashedPassword,
     };
@@ -72,7 +87,7 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     const { username, password } = req.body;
 
-    let sql = "SELECT password FROM users WHERE username = ?";
+    let sql = "SELECT password, user_id FROM users WHERE username = ?";
 
     let query = await new Promise((resolve, reject) => {
         conn.query(sql, username, (err, result) => {
@@ -83,18 +98,24 @@ const login = async (req, res) => {
     });
 
     if (query.length < 1) {
-        return res.render("login", { message: "That user does not exist!" });
+        return res.render("login", {
+            invalidUser: "That user does not exist!",
+        });
     }
 
-    let checkPassword = await bcrypt.compare(password, query[0].password);
+    const queryPassword = query[0].password;
+    const queryUserID = query[0].user_id;
+
+    let checkPassword = await bcrypt.compare(password, queryPassword);
 
     if (!checkPassword)
-        return res.render("login", { message: "Invalid Password!" });
+        return res.render("login", { invalidPassword: "Invalid Password!" });
 
     if (!req.session.authenticated) {
         req.session.authenticated = true;
-        req.session.user = { username };
-        return res.render("login", { message: "Successfully Logged in!" });
+        req.session.user = { queryUserID };
+        console.log(req.session.user);
+        return res.redirect("/");
     }
 
     res.render("login", { message: "You're already logged in!" });
