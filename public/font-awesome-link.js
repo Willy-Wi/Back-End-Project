65a26bea5a4f8859d55013866e7948b850ebae80
Willy Wijaya <wijayawilly2003@gmail.com>
2021-12-13 22:19:24 +0700

    Delete and move file to a new folder

--- public/font-awesome-link.js
+++
@@ -1,6 +0,0 @@
-// Append Link to all HTML
-// Biar gak capek copy paste tiap kali buat HTML baru
-
-const headContent = document.getElementsByTagName("head")[0];
-let font_link = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">`;
-headContent.insertAdjacentHTML("beforeend", font_link);
