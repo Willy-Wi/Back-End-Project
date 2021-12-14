017c1a7a71f78da5c4a2684a146d69a2fc9b702c
Willy Wijaya <wijayawilly2003@gmail.com>
2021-12-14 22:15:24 +0700

    Add another route and more security

--- routes/pages.js
+++ routes/pages.js
@@ -13,35 +13,71 @@ const conn = mysql2.createConnection({
     database: "test_db_home",
 });
 
-router.get("/", async (req, res) => {
-    let sql = "SELECT * FROM posts";
+router.get("/search", async (req, res) => {
+    const filter = req.query.search
+    if (filter < 1) return res.redirect("/");
+
+    let sql = `
+        SELECT Users.username, Users.name, Posts.post_text
+        FROM Users INNER JOIN Posts ON Users.user_id = Posts.user_id
+        WHERE Users.username LIKE '%${filter}%' OR
+        Users.name LIKE '%${filter}%' OR
+        Posts.post_text LIKE '%${filter}%'
+    `;
 
     let query = await new Promise((resolve, reject) => {
         conn.query(sql, (err, result) => {
-            if (err) reject(err);
-
+            if (err) reject(err) ;
             resolve(result);
         });
     });
 
-    if (!req.session.authenticated)
+    if (!req.session.authenticated) {
         return res.render("home", { login: false, result: query });
+    }
+
+    res.render("home", { result: query, login: true });
+});
+
+router.get("/", async (req, res) => {
+    let sql =
+        "SELECT Users.username, Users.name, Posts.post_text FROM Posts INNER JOIN Users ON Users.user_id = Posts.user_id";
+
+    let query = await new Promise((resolve, reject) => {
+        conn.query(sql, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+
+    if (!req.session.authenticated) {
+        return res.render("home", { login: false, result: query });
+    }
 
     res.render("home", { result: query, login: true });
 });
 
 router.get("/create", (req, res) => {
-    if (!req.session.authenticated)
+    if (!req.session.authenticated) {
         return res.render("login", { message: "You have not logged in!" });
+    }
 
     res.render("create_post", { login: true });
 });
 
 router.get("/register", (req, res) => {
+    if (req.session.authenticated) {
+        return res.redirect("/");
+    }
+
     res.render("register");
 });
 
 router.get("/login", (req, res) => {
+    if (req.session.authenticated) {
+        return res.redirect("/");
+    }
+
     res.render("login");
 });
 
@@ -50,17 +86,18 @@ router.get("/logout", (req, res) => {
         req.session.authenticated = false;
         return res.redirect("/");
     }
+
     res.render("login", { message: "You are not logged in!" });
 });
 
-const validationLogin = [
+const validationRegister = [
     check("username")
         .isLength({ min: 3 })
         .withMessage("Username must be at least 3 characters"),
     check("password")
         .isLength({ min: 5 })
         .withMessage("Password must be at least 5 characters"),
-    check("email").isEmail().withMessage("Email is not valid"),
+    check("email").isEmail().withMessage("Please enter an email"),
 ];
 
 const validatePost = [
@@ -72,7 +109,7 @@ const validatePost = [
 ];
 
 router.post("/create", validatePost, create_post);
-router.post("/register", validationLogin, register);
+router.post("/register", validationRegister, register);
 router.post("/login", login);
 
 module.exports = router;
