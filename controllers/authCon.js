effa8a45911b8d03f3df6a9874bb3fddcd2f00af
Willy Wijaya <wijayawilly2003@gmail.com>
2021-12-16 21:59:31 +0700

    Move postCreate to a separate file
    I moved postCreate route to a separate file in the controllers folder.
    I also refactored some code to make it easier to understand.
    Rename some variables for readability.
    All query functions can now be called from a single file to avoid code repetition.

--- controllers/authCon.js
+++ controllers/authCon.js
@@ -1,98 +1,60 @@
 const bcrypt = require("bcrypt");
 const { validationResult } = require("express-validator");
-const { conn } = require("../routes/database_conn");
+const { query } = require("../routes/dbFunctions");
 // const isEligibleRequest = require("express-fileupload/lib/isEligibleRequest");
 
-function query(sql, data) {
-    return new Promise((resolve, reject) => {
-        conn.query(sql, data, (err, res) => {
-            if (err) reject(err);
-            resolve(res);
-        });
-    });
-}
-
-const create_post = (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.render("create_post", {
-            message: errors.array(),
-            login: true,
-        });
-    }
-
-    const { post_message } = req.body;
-    const { user_id } = req.session.user;
-
-    if (!req.session.authenticated) return res.render("login");
-
-    let sql = "INSERT INTO posts SET ?";
-
-    let data = {
-        user_id: user_id,
-        post_text: post_message,
-    };
-
-    query(sql, data).then(() => res.redirect("/"));
-
-};
-
+// * Register HTTP Post
 const register = async (req, res) => {
     const errors = validationResult(req);
     const { username, fullname, email, password, confirmPassword } = req.body;
-    let sql = `SELECT email, username FROM users WHERE BINARY email = '${email}' OR username = '${username}'`;
+    let sql = `SELECT email, username FROM users WHERE BINARY email = '${email}' OR username = '@${username}'`;
 
+    let errUser, errEmail;
     let regex = /[^A-Za-z0-9_]/g;
-
-    // Querying to see if the data already exists or not
-    let query = await new Promise((resolve, reject) => {
-        conn.query(sql, (err, result) => {
-            if (err) reject(err);
-
-            resolve(result);
-        });
-    });
+    let result = await query(sql);
+    let resultValue = result.length > 0;
+    let items = {};
 
     // Validating Data
-    if (!errors.isEmpty() || query.length > 0) {
-        let error = errors.array();
-        let errorUsername, errorEmail, errorPassword, invalidPasswordMatch;
-        items = {}
-
-        for (let i of error) {
+    if (!errors.isEmpty() || resultValue) {
+        for (let i of errors.array()) {
             items[i.param] = i.msg;
-        };
+        }
 
-        if (items.username) {
-            errorUsername = items.username;
+        const {
+            username: invalidUser,
+            email: invalidEmail,
+            password: invalidPass,
+        } = items;
+
+        if (invalidUser) {
+            errUser = invalidUser;
         } else if (username.match(regex)) {
-            errorUsername =
-                "Username can only have numbers, letters, and underscores";
-        } else if (query.length > 0 && query[0].username) {
-            errorUsername = "That username already exist!";
+            errUser =
+                "Username can only contain numbers, letters, and underscores";
+        } else if (resultValue && result[0].username) {
+            errUser = "That username already exist!";
         }
 
-        if (items.email) {
-            errorEmail = items.email;
-        } else if (query.length > 0 && query[0].email) {
-            errorEmail = "That email is already registered";
+        console.log(errUser);
+
+        if (invalidEmail) {
+            errEmail = invalidEmail;
+        } else if (resultValue && result[0].email) {
+            errEmail = "That email is already registered";
         }
 
-        if (items.password) {
-            errorPassword = items.password;
-        }
+        let errPass = invalidPass ? invalidPass : null;
 
         // Checking if Password is equal to Confirm Password Field
-        if (password !== confirmPassword) {
-            invalidPasswordMatch = "Password does not match!";
-        }
+        let errPassMatch =
+            password !== confirmPassword ? "Password does not match!" : null;
 
         return res.render("register", {
-            errorUsername,
-            errorEmail,
-            errorPassword,
-            invalidPasswordMatch,
+            errUser,
+            errEmail,
+            errPass,
+            errPassMatch,
         });
     }
 
@@ -106,46 +68,37 @@ const register = async (req, res) => {
         password: hashedPassword,
     };
 
-    conn.query(sql, data, (err, result) => {
-        if (err) throw err;
-    });
-    res.render("login");
+    await query(sql, data);
+
+    res.redirect("/login");
 };
 
+// * Login HTTP Post
 const login = async (req, res) => {
     const { email, password } = req.body;
 
     let sql = "SELECT password, user_id FROM users WHERE BINARY email = ?";
 
-    let query = await new Promise((resolve, reject) => {
-        conn.query(sql, email, (err, result) => {
-            if (err) reject(err);
+    let result = await query(sql, email);
 
-            resolve(result);
-        });
-    });
-
-    if (query.length < 1) {
+    if (result.length < 1) {
         return res.render("login", {
             invalidEmail: "That user does not exist!",
         });
     }
 
-    const qPassword = query[0].password;
-    const qUserID = query[0].user_id;
-
-    let checkPassword = await bcrypt.compare(password, qPassword);
+    let checkPassword = await bcrypt.compare(password, result[0].password);
 
     if (!checkPassword)
         return res.render("login", { invalidPassword: "Invalid Password!" });
 
-    if (!req.session.authenticated) {
-        req.session.authenticated = true;
+    if (!req.session.loggedIn) {
+        req.session.loggedIn = true;
         req.session.user = {
-            user_id: qUserID,
+            user_id: result[0].user_id,
         };
         return res.redirect("/");
     }
 };
 
-module.exports = { register, login, create_post };
+module.exports = { register, login };
