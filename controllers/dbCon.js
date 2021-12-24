a6c64e805cc8b682c22e436cb269adf8e3cb7ddf
Willy-Wi <86144589+Willy-Wi@users.noreply.github.com>
2021-12-24 23:20:35 +0700

    Add .env feature for security reasons

--- controllers/dbCon.js
+++ controllers/dbCon.js
@@ -1,10 +1,11 @@
+require("dotenv").config({ path: __dirname + "/.env"});
 const mysql2 = require("mysql2");
 
 const conn = mysql2.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "willywijaya5520",
-    database: "popsicle",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
 });
 
 function query(sql, data = null) {
