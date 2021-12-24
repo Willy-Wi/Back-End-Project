3d83e12a4e99e41f6ee64d8e04b42bcca74f45fd
Willy-Wi <86144589+Willy-Wi@users.noreply.github.com>
2021-12-24 23:14:58 +0700

    Add script and view for comment feature

--- controllers/createPost.js
+++ controllers/createPost.js
@@ -6,11 +6,24 @@ const createPost = (req, res) => {
     let data = {
         user_id: req.session.user_id,
         post_title: title,
-        post_text: description
+        post_content: description
     }
     query(sql, data);
     
     res.redirect("/");
 }
 
-module.exports = { createPost }
+const createComment = (req, res) => {
+    const postId = req.params.id;
+    const { comment } = req.body;
+    let sql = 'INSERT INTO comments SET ?';
+    let data = {
+        user_id: req.session.user_id,
+        post_id: postId,
+        comment_text: comment,
+    }
+    query(sql, data);
+    res.redirect("/");
+}
+
+module.exports = { createPost, createComment }
