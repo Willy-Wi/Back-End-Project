65e7c88760619d872f4e52e79b04284ecbadb0e9
Willy-Wi <86144589+Willy-Wi@users.noreply.github.com>
2021-12-25 20:10:26 +0700

    Add a script for Following

---
+++ public/js/follow.js
@@ -0,0 +1,19 @@
+let updateStat = {
+    Follow: function (button) {
+        button.dataset.followStatus = "Unfollow";
+        button.classList.toggle("profile__follow--active");
+    },
+    Unfollow: function (button) {
+        button.dataset.followStatus = "Follow";
+        button.classList.toggle("profile__follow--active");
+    },
+};
+
+function follow (event) {
+    console.log("Pass");
+    let action = event.target.dataset.followStatus;
+    let userId = event.target.dataset.userId;
+
+    updateStat[action](event.target);
+    axios.post("/users/" + userId + "/act", { action: action });
+}
