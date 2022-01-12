fc2ce803e108da48d2f5a69ee1682a9e482f3185
Willy Wijaya <wijayawilly2003@gmail.com>
2022-01-12 17:31:18 +0700

    Add additional code and bug fix

--- controllers/editComment.js
+++ controllers/editComment.js
@@ -1,20 +1,20 @@
 const { query } = require("./dbCon");
 //Delete Comment
 const deleteComment = (req, res) => {
-    const deleteId = req.params.id;
-    let sql = `delete from comments where comment_id = ` + deleteId;
+    const commentId = req.params.id;
+    let sql = `DELETE FROM comments WHERE comment_id = '${commentId}'`;
     query(sql);
     res.end();
 };
 
 const editComment = async (req, res) => {
-    const { edit_comment } = req.body;
+    const { edit_comment, postId } = req.body;
     const commentId = req.params.id;
     let sql = `UPDATE comments SET comment_content = '${edit_comment}' WHERE comment_id = '${commentId}'`;
 
     await query(sql);
 
-    res.json({ redirect: "/" })
+    res.json({ redirect: "/posts/" + postId });
 };
 
 module.exports = { deleteComment, editComment };
