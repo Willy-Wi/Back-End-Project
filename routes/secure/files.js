552ae211e32fbd4788956f579076d5f7e1dd4271
Willy-Wi <86144589+Willy-Wi@users.noreply.github.com>
2022-01-23 17:55:29 +0700

    Bug Fix and things I forgot

--- routes/secure/files.js
+++ routes/secure/files.js
@@ -6,19 +6,19 @@ const { uploadFiles } = require("../../controllers/filesCon");
 const { isLoggedIn } = require("../../controllers/middleware/middleware");
 
 router.get("/album/:id", async (req, res) => {
-    const albumid = req.params.id;
+    const albumId = req.params.id;
 
-    let sql = `SELECT file_id, album_id, file_url, file_type, thumb_url FROM files WHERE album_id = '${albumid}' ORDER BY created_at DESC`;
+    let sql = `SELECT file_id, album_id, file_url, file_type, thumb_url FROM files WHERE album_id = '${albumId}' ORDER BY created_at DESC`;
     let files = await query(sql);
 
-    sql = `SELECT Users.name, Users.username, Users.user_id, Users.email, Users.profile_image FROM Users INNER JOIN Albums ON Albums.user_id = Users.user_id WHERE Albums.album_id = ${albumid}`;
+    sql = `SELECT Users.name, Users.username, Users.user_id, Users.email, Users.profile_image FROM Users INNER JOIN Albums ON Albums.user_id = Users.user_id WHERE Albums.album_id = ${albumId}`;
     let userInfo = await query(sql);
 
-    sql = `SELECT album_name, album_cover, album_description FROM albums WHERE album_id = '${albumid}'`;
+    sql = `SELECT album_name, album_cover, album_description FROM albums WHERE album_id = '${albumId}'`;
     let album = await query(sql);
 
     res.render("profile/addfiles", {
-        albumid: albumid,
+        albumId,
         album: album[0],
         files: files,
         file: files[0],
