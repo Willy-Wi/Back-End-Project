4f93d217b5958bf06d76ec8475e19c601398025c
Willy-Wi <86144589+Willy-Wi@users.noreply.github.com>
2022-01-23 12:46:12 +0700

    Bug Fix

--- routes/secure/posts/postPages.js
+++ routes/secure/posts/postPages.js
@@ -4,9 +4,10 @@ const { query } = require("../../../controllers/dbCon");
 const {
     isNotLoggedIn,
     isLoggedIn,
+    loginRequired,
 } = require("../../../controllers/middleware/middleware");
 
-router.get("/featured-post", isNotLoggedIn, async (req, res) => {
+router.get("/featured-post", isNotLoggedIn, loginRequired, async (req, res) => {
     let sql = `SELECT Users.username, Users.user_id, Users.profile_image, Posts.post_title, Posts.post_file , Posts.post_content, Posts.post_id, COUNT(Likes.user_id) AS 'likes'
     FROM Users INNER JOIN Posts ON Posts.user_id = Users.user_id
     LEFT JOIN Likes ON Likes.post_id = Posts.post_id GROUP BY Posts.post_id ORDER BY COUNT(Likes.post_id) DESC LIMIT 5;`;
@@ -21,7 +22,7 @@ router.get("/featured-post", isNotLoggedIn, async (req, res) => {
     });
 });
 
-router.get("/mytopics", isLoggedIn, async (req, res) => {
+router.get("/mytopics", isLoggedIn, loginRequired, async (req, res) => {
     let sql = `SELECT Users.username, Users.user_id, Users.profile_image, Posts.post_title, Posts.post_file ,Posts.post_content, Posts.post_id, COUNT(Likes.user_id) AS 'likes'
     FROM Users INNER JOIN Posts ON Posts.user_id = Users.user_id
     LEFT JOIN Likes ON Likes.post_id = Posts.post_id WHERE Users.user_id = '${req.session.user.user_id}' GROUP BY Posts.post_id;`;
@@ -37,11 +38,8 @@ router.get("/mytopics", isLoggedIn, async (req, res) => {
 });
 
 router.get("/myanswers", isLoggedIn, async (req, res) => {
-    const postId = req.params.id;
-
-    sql = `SELECT Users.username, Users.user_id, Comments.comment_content FROM Users INNER JOIN Comments ON
+    sql = `SELECT Users.username, Users.user_id, Comments.comment_id, Comments.post_id, Comments.comment_content FROM Users INNER JOIN Comments ON
     Users.user_id = Comments.user_id WHERE Users.user_id = '${req.session.user.user_id}' ORDER BY Comments.created_at DESC`;
-
     let comments = await query(sql);
 
     res.render("posts/myanswers", {
